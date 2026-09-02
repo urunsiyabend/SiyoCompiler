5162b33edd26d3ff9a353f5cb0a7acac58fa8586
Anubyss-mihail <anubyssanubyss@gmail.com>
2026-09-02 23:18:39 +0200

    Format SiyoMapTest

--- src/test/java/codeanalysis/SiyoMapTest.java
+++ src/test/java/codeanalysis/SiyoMapTest.java
@@ -1,30 +1,30 @@
- package codeanalysis;
+package codeanalysis;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-    public class SiyoMapTest {
+public class SiyoMapTest {
 
-        @Test
-        void storesAndRetrievesValues() {
-            SiyoMap map = new SiyoMap();
+    @Test
+    void storesAndRetrievesValues() {
+        SiyoMap map = new SiyoMap();
 
-            map.set("language", "Siyo");
+        map.set("language", "Siyo");
 
-            assertTrue(map.has("language"));
-            assertEquals("Siyo", map.get("language"));
-            assertEquals(1, map.size());
-            assertNull(map.get("missing"));
-        }
-
-        @Test
-        void incrementsIntegerValues() {
-            SiyoMap map = new SiyoMap();
-
-            map.increment("visits");
-            map.increment("visits");
-
-            assertEquals(2, map.getInt("visits"));
-        }
+        assertTrue(map.has("language"));
+        assertEquals("Siyo", map.get("language"));
+        assertEquals(1, map.size());
+        assertNull(map.get("missing"));
     }
+
+    @Test
+    void incrementsIntegerValues() {
+        SiyoMap map = new SiyoMap();
+
+        map.increment("visits");
+        map.increment("visits");
+
+        assertEquals(2, map.getInt("visits"));
+    }
+}
